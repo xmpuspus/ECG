c3193fa9a7e84ebbea6febc6fec018d385a3b46c
mventura <mventura@savvysherpa.com>
2017-03-22 19:32:41 +0800

    Add calculation of hr, hrv, edr, rr

--- savvyecg/ecg_utils.py
+++ savvyecg/ecg_utils.py
@@ -10,6 +10,7 @@ from scipy.ndimage import convolve1d
 import itertools
 from imblearn.over_sampling import SMOTE
 from biosppy.signals import ecg
+import sensorlib.freq as slf
 
 
 ## process ECG signal 
@@ -20,7 +21,7 @@ def baseline_correct(x, fs):
     
     Parameters
     ----------
-    x : array_like
+    x: array_like
         Array containing magnitudes of ECG signal
 
     fs: float
@@ -45,7 +46,7 @@ def smooth(x, fs, order = 1, btype = 'low', corner_freq_hz = 150):
     
     Parameters
     ----------
-    x : array_like
+    x: array_like
         Array containing magnitudes of ECG signal
 
     fs: float
@@ -85,7 +86,7 @@ def preprocess_ecg(x, fs, lp_cornerfreq = 40):
     
     Parameters
     ----------
-    x : array_like
+    x: array_like
         Array containing magnitudes of ECG signal
 
     fs: float
@@ -169,7 +170,7 @@ def make_ecg_dataframe(x, fs, col_name = 'ecg'):
     
     Parameters
     ----------
-    x : array_like
+    x: array_like
         Array containing magnitudes of ECG signal
 
     fs: float
@@ -197,8 +198,8 @@ def process_ecg_df(df, fs, lp_cornerfreq = 40, input_col = 'ecg', output_col = '
     
     Parameters
     ----------
-    df : pandas data frame
-        data frame containing the magnitudes of th raw ECG signal in one column
+    df: pandas data frame
+        data frame containing the magnitudes of the raw ECG signal in one column
 
     fs: float
         Number corresponding to the sampling frequency of the input signal,
@@ -233,5 +234,123 @@ def process_ecg_df(df, fs, lp_cornerfreq = 40, input_col = 'ecg', output_col = '
     else:
         return df
         
+def edr(sig, rpeaks, interp = 'linear'):
+    """
+    Calculate the ECG-derived respiration (EDR)
+    
+    Parameters
+    ----------
+    sig: array_like
+        Array containing magnitudes of ECG signal
+        
+    rpeaks: array_like
+        Array containing indices of the R peaks in the ECG signal
+    
+    interp: string, optional
+        Specifies the kind of interpolation as a string(‘linear’, ‘nearest’, ‘zero’, 
+        ‘slinear’, ‘quadratic’, ‘cubic')
+        
+    Returns
+    -------
+    edr_sig: array
+        Array containing the interpolated ECG-derived respiration signal
+        
+    rpeaks_interp: array
+        Array containing the interpolated R-peak locations
+    
+    """
+    f = si.interp1d(rpeaks, sig[rpeaks], kind=interp)
+    rpeaks_interp = np.arange(rpeaks[0], rpeaks[-1]+1)
+    edr_sig = f(rpeaks_interp)
+    edr_sig -= np.mean(edr_sig)
+    return edr_sig, rpeaks_interp
+
+def resp_rate(edr_sig, fs, unit = 'persec'):
+    """
+    Calculate an estimate of the respiration rate from EDR
+    
+    Parameters
+    ----------
+    edr_sig: array_like
+        Array containing magnitudes of EDR signal       
+    
+    fs: float
+        Number corresponding to the sampling frequency of the input signal,
+        must be in Hertz
+        
+    unit: string, optional 
+        Specifies unit of respiration rate ('persec' refers to breaths per second,
+        and 'permin' refers to breaths per minute)
+        
+    Returns
+    -------
+    rr: float
+        Estimate of the respiration rate
+        
+    """
+    
+    multiplier = {'persec': 1, 'permin': 60}
+    rr = slf.fund_freq_by_corr(edr_sig, samp_freq = fs)*multiplier[unit]
+    return rr
+                
+    
+def heart_rate(r_peaks, fs, unit = 'persec'):
+    """
+    Calculate heart rate from the location of R peaks
+    
+    Parameters
+    ----------
+    rpeaks: array_like
+        Array containing indices of the R peaks in the ECG signal  
+    
+    fs: float
+        Number corresponding to the sampling frequency of the input signal,
+        must be in Hertz
+        
+    unit: string, optional 
+        Specifies unit of heart rate ('persec' refers to breaths per second,
+        and 'permin' refers to breaths per minute)
+        
+    Returns
+    -------
+    hr: float
+        Estimate of the heart rate
+        
+    """ 
+    multiplier = {'persec': 1, 'permin': 60}
+    time_peaks = r_peaks/fs
+    dt = time_peaks[1:] - time_peaks[:-1]
+    hr = 1/np.nanmean(dt) *multiplier[unit]
+    return hr
+
+def heart_rate_var(r_peaks, fs, unit = {'ms'}):
+    """
+    Calculate heart rate from the location of R peaks
+    
+    Parameters
+    ----------
+    rpeaks: array_like
+        Array containing indices of the R peaks in the ECG signal  
+    
+    fs: float
+        Number corresponding to the sampling frequency of the input signal,
+        must be in Hertz
+        
+    unit: string, optional 
+        Specifies unit of heart rate ('s' refers to second,
+        and 'ms' refers to millisecond)
+        
+    Returns
+    -------
+    hrv: float
+        Estimate of the heart rate variability
+        
+    """     
+    multiplier = {'s': 1, 'ms': 1000}    
+    time_peaks = r_peaks/fs
+    dt = time_peaks[1:] - time_peaks[:-1]
+    hrv = np.nanstd(dt)*multiplier[unit]
+    return hrv
+    
 
 
